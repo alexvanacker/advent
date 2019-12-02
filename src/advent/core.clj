60160583168abfc3d45de2e830e2e557948f9543
Alexis Vanacker <alexis.vanacker@concordnow.com>
2019-12-02 21:48:55 +0000

    Format core.clj

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -10,7 +10,7 @@
         [(conj freq_set res) res])))
 
 (defn find_first_dup [freqs start input]
-   (reduce sum_reduce [freqs start] input))
+  (reduce sum_reduce [freqs start] input))
 
 (defn sol [input]
   (loop [freqs #{0} start 0]
@@ -19,11 +19,6 @@
       (if (== (count res) 1)
         res
         (recur (set/union freqs (get res 0)) (get res 1))))))
-
-(defn read-file [f]
-  (-> (slurp f)
-      (clojure.string/split-lines)))
-
 
 (defn contains-exact-n? [elts n]
   (contains? (set (vals (frequencies elts)))
@@ -34,13 +29,11 @@
    (fn [{:keys [two three]} x]
      (cond
        (and
-          (contains-exact-n? x 2)
-          (contains-exact-n? x 3)) {:two (inc two), :three (inc three)}
+        (contains-exact-n? x 2)
+        (contains-exact-n? x 3)) {:two (inc two), :three (inc three)}
        (contains-exact-n? x 2) {:two (inc two), :three three}
        (contains-exact-n? x 3) {:two two, :three (inc three)}
-       :else {:two two, :three three}
-       )) {:two 0, :three 0} elts))
-
+       :else {:two two, :three three})) {:two 0, :three 0} elts))
 
 (defn sol20191 [mass]
   (- (quot mass 3)
@@ -54,12 +47,12 @@
 (defn mod_fuel_requirements
   ([mass]
    (mod_fuel_requirements (sol20191 mass) (list (sol20191 mass))))
-   ([mass reqs]
-    (let [req (sol20191 mass)]
-      (println (str "computin mass " mass))
-      (if (>= 0 req)
-        (reduce + reqs)
-        (recur req (conj reqs req))))))
+  ([mass reqs]
+   (let [req (sol20191 mass)]
+     (println (str "computin mass " mass))
+     (if (>= 0 req)
+       (reduce + reqs)
+       (recur req (conj reqs req))))))
 
 (defn sol2019_2_complete []
   (reduce +
