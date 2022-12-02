25d2e379808645b5f42cf6dd6cd8fe3cd980542e
Alexis Vanacker <alexis.vanacker@akur8.com>
2022-12-02 13:44:21 +0100

    Fix utils

--- src/advent/utils.clj
+++ src/advent/utils.clj
@@ -1,12 +1,13 @@
 (ns advent.utils
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (defn read-file
   " Function that returns the lines as an array found in the file
   located in resources/`filename`"
   [filename]
   (-> (slurp (io/resource filename))
-      (clojure.string/split-lines)))
+      (str/split-lines)))
 
 (defn read-file-ints
   [filename]
@@ -17,17 +18,17 @@
   "Function that reads a file that represents an array of integers in one line and returns that array."
   [filename]
   (-> (slurp (io/resource filename))
-      (clojure.string/trim)
-      (#(clojure.string/split % #","))))
+      (str/trim)
+      (#(str/split % #","))))
 
 
 (defn split-comma [string]
-  (clojure.string/split string #","))
+  (str/split string #","))
 
 (defn read-file-lines-split
   "Reads a file, and splits each line with `,`."
   [filename]
   (as-> (slurp (io/resource filename)) v
-      (clojure.string/split-lines v)
+      (str/split-lines v)
       (map split-comma v)))
  
